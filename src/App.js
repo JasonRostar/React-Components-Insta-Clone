966156456eb56d6e41bbd306b49cf7cf798f8015
Jason <jason.rostar@yahoo.com>
2020-04-14 15:53:18 -0700

    re-cloned and icons work now

--- src/App.js
+++ src/App.js
@@ -4,16 +4,23 @@ PostsContainer components and the CommentSection Components.
 Not all files need code added. 
 Look at each file to see where you need to pass props or add code 
 */
-import React from "react";
+import React, { useState} from "react";
 import "./App.css";
+
 // import the PostsPage and SearchBar and add them to the App
+import PostPage from "./components/PostsContainer/PostsPage"
+import SearchBar from "./components/SearchBar/SearchBarContainer"
+import data from "./dummy-data"
 
 
 const App = () => {
-
+const [users] = useState(data)
   return (
     <div className="App">
       {/* Add imported components here to render them */}
+      <SearchBar />
+      <PostPage data={users} />
+
     </div>
   );
 };
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -3,15 +3,24 @@
 import React from "react";
 import Post from "./Post";
 import "./Posts.css";
+
 // import data 
 
-const PostsPage = () => {
-  // set up state for your data
+
+const PostsPage = ({ data }) => {
+  
   return (
     <div className="posts-container-wrapper">
-      {/* map through data here to return a Post and pass data as props to Post */}
+
+
+    {
+    data.map(user => {
+      return <Post userData={user} />
+    })
+    }
     </div>
   );
+
 };
 
 export default PostsPage;
