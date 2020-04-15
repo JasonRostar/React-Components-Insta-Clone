25388b65a8a327b14369380965c06c908c352c8c
Jason <jason.rostar@yahoo.com>
2020-04-14 19:46:19 -0700

    finished mvps

--- src/components/CommentSection/Comment.js
+++ src/components/CommentSection/Comment.js
@@ -1,14 +1,15 @@
+  
 // You do not need to do anything in this file. Look at the CommentSection file. 
 
 import React from 'react';
 import './Comment.css';
 
-const Comment = props => {
+const Comment = ({ username, text }) => {
   return (
     <div className="comment-text">
-      <span className="user">{props.comment.username}</span>
+      <span className="user">{username}</span>
       {' '}
-      <span className="comment">{props.comment.text}</span>
+      <span className="comment">{text}</span>
     </div>
   );
 };
